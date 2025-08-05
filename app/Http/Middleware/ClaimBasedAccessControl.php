7636a006f2feacba48bdd46de68f20cf32dd2312
Loki Sinclair <loki.sinclair@hdruk.ac.uk>
2025-08-05 12:08:27 +0100

    feat(GAT-7670): Workgroups being passed from Gateway to Daphne and honoured via endpoint protection via middleware

--- app/Http/Middleware/ClaimBasedAccessControl.php
+++ app/Http/Middleware/ClaimBasedAccessControl.php
@@ -13,8 +13,14 @@ use Lcobucci\JWT\Signer\Key\InMemory;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
+use Hdruk\ClaimsAccessControl\Services\ClaimMappingService;
+use Hdruk\ClaimsAccessControl\Services\ClaimResolverService;
+
+use App\Traits\Responses;
+
 class ClaimBasedAccessControl
 {
+    use Responses;
     /**
      * Handle an incoming request.
      *
@@ -22,9 +28,49 @@ class ClaimBasedAccessControl
      */
     public function handle(Request $request, Closure $next, ...$claims): Response
     {
-        // TODO - As this requires Gateway being up to date with workgroups,
-        // which it currently is not, we will stub this out for now.
+        // Decode JWT token and check claims
+        $request->header('Authorization');
+        $token = $request->bearerToken();
 
-        return $next($request);
+        $signer = new Sha256();
+        $key = InMemory::plainText(config('gateway.jwt_secret'));
+
+        $config = Configuration::forSymmetricSigner($signer, $key);
+        $token = $config->parser()->parse($token);
+
+        $user = $token->claims()->get('user');
+
+        $claimMappingService = new ClaimMappingService();
+        $claimResolverService = new ClaimResolverService($claimMappingService);
+
+        // normalise the workgroup claims to determine access
+        $newArr = $this->normaliseWorkgroups($user['workgroups']);
+        unset($user['workgroups']);
+        $user['workgroups'] = $newArr['workgroups'];
+
+        foreach ($claims as $claim) {
+            $resolution = $claimResolverService->hasWorkgroup($user, config('claims-access.default_system'), $claim);
+
+            if ($resolution) {
+                return $next($request);
+            }
+        }
+
+        // Return unauthorized response if claim is not found
+        return $this->UnauthorisedResponse();
     }
+
+    public function normaliseWorkgroups(array $data): array
+    {
+        $normalised = [];
+
+        foreach ($data as $workgroup) {
+            if (is_array($workgroup) && isset($workgroup['name'])) {
+                $normalised[] = strtolower($workgroup['name']);
+            }
+        }
+
+        return ['workgroups' => [config('claims-access.default_system') => $normalised]];
+    }
+
 }
