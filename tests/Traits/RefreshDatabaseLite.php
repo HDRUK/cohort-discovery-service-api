f560f97b9c18d56553011e248043ff7a61ccd213
Calum Macdonald <calmacx@gmail.com>
2025-09-23 15:47:57 +0100

    use omop db seeder

--- tests/Traits/RefreshDatabaseLite.php
+++ tests/Traits/RefreshDatabaseLite.php
@@ -9,6 +9,7 @@ trait RefreshDatabaseLite
 {
     protected static bool $migrated = false;
     protected static $databaseConnection = null;
+    protected static $omopConnection = null;
 
     public function liteSetUp(): void
     {
@@ -17,23 +18,34 @@ trait RefreshDatabaseLite
         if (!static::$migrated) {
             Artisan::call('migrate');
             Artisan::call('db:seed', ['--class' => 'DatabaseSeeder']);
+
+            Artisan::call('db:seed', [
+                '--class'    => 'SimpleOmopSeeder',
+                '--database' => 'omop',
+            ]);
+
+
             static::$migrated = true;
 
             // Store the connection (for SQLite in-memory)
             static::$databaseConnection = DB::connection()->getPdo();
+            static::$omopConnection     = DB::connection('omop')->getPdo();
         }
 
         // Reuse the same connection across tests (fix for SQLite in-memory)
         DB::connection()->setPdo(static::$databaseConnection);
+        DB::connection('omop')->setPdo(static::$omopConnection);
 
         // Start a manual transaction
         DB::beginTransaction();
+        DB::connection('omop')->beginTransaction();
     }
 
     public function tearDown(): void
     {
         // Rollback after each test
         DB::rollBack();
+        DB::connection('omop')->rollBack();
 
         parent::tearDown();
     }
