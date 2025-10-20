bde1fcbdfaad55490c9842a69ec16c43a8460e93
Calum Macdonald <calmacx@gmail.com>
2025-10-20 09:59:39 +0100

    revert

--- app/Models/Omop/ConceptAncestor.php
+++ app/Models/Omop/ConceptAncestor.php
@@ -14,7 +14,7 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 class ConceptAncestor extends Model
 {
     protected $connection = 'omop';
-    protected $table = 'omop.concept_ancestor';
+    protected $table = 'concept_ancestor';
     public $timestamps = false;
     public $incrementing = false;
 
