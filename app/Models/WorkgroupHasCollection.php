4dd552a064ce45991ecc897b0c82af5727fab494
Calum Macdonald <calmacx@gmail.com>
2025-12-03 13:07:03 +0000

    missing swagger

--- app/Models/WorkgroupHasCollection.php
+++ app/Models/WorkgroupHasCollection.php
@@ -2,9 +2,33 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\Pivot;
 
+/**
+ * @OA\Schema(
+ *     schema="WorkgroupHasCollection",
+ *     type="object",
+ *     title="WorkgroupHasCollection",
+ *     description="Pivot linking a workgroup to a collection.",
+ *     required={"workgroup_id","collection_id"},
+ *
+ *     @OA\Property(
+ *         property="workgroup_id",
+ *         type="integer",
+ *         example=1,
+ *         description="ID of the workgroup"
+ *     ),
+ *     @OA\Property(
+ *         property="collection_id",
+ *         type="integer",
+ *         example=1,
+ *         description="ID of the collection"
+ *     )
+ * )
+ *
+ * @property int $workgroup_id
+ * @property int $collection_id
+ */
 class WorkgroupHasCollection extends Pivot
 {
     protected $table = 'workgroup_has_collection';
