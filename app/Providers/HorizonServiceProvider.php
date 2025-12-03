f07ab0409b81529eb2f73ecc0ba5feeecc4b93ad
Calum Macdonald <calmacx@gmail.com>
2025-12-03 12:40:56 +0000

    temp for horizon

--- app/Providers/HorizonServiceProvider.php
+++ app/Providers/HorizonServiceProvider.php
@@ -28,9 +28,9 @@ class HorizonServiceProvider extends HorizonApplicationServiceProvider
     protected function gate(): void
     {
         Gate::define('viewHorizon', function ($user = null) {
-            return in_array(optional($user)->email, [
+            return true;/* in_array(optional($user)->email, [
                 //
-            ]);
+            ]);*/
         });
     }
 }
