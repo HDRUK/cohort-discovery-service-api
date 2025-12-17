35e77abefcdd72659df246eb4a4906e0519f780d
Calum Macdonald <calmacx@gmail.com>
2025-12-17 16:45:19 +0000

    bug fix

--- app/Models/CollectionConfig.php
+++ app/Models/CollectionConfig.php
@@ -72,7 +72,7 @@ class CollectionConfig extends Model implements ValidatableModel
             ],
             'store' => [
                 'collection_id' => 'required|exists:collections,id',
-                'run_time_hour' => 'required|integer|min:0|max:12',
+                'run_time_hour' => 'required|integer|min:0|max:23',
                 'run_time_minute' => 'required|integer|min:0|max:59',
                 'frequency_mode' => 'required|integer',
                 'run_time_frequency' => 'required|integer',
