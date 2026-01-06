a9005a56e449d8ab570394870f88c6550cdd645f
Calum Macdonald <calmacx@gmail.com>
2026-01-06 10:16:14 +0000

    fix .env.example

--- config/tasks.php
+++ config/tasks.php
@@ -3,5 +3,5 @@
 return [
     'default_max_attempts' => env('MAX_ATTEMPTS', 3),
     'default_lease_seconds' => env("LEASE_SECONDS", 60),
-    'default_timeout_seconds' => env('TASK_TIMEOUT_SECONDS', 5),
+    'default_timeout_seconds' => env('TASK_TIMEOUT_SECONDS', 300),
 ];
