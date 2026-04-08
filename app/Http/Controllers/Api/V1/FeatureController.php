8c948b94063d4b0c3677cbb3c7b8068b8ad464da
Calum Macdonald <69473770+calmacx@users.noreply.github.com>
2026-04-08 14:18:21 +0100

    feat(DP-678): Adding activity logger for admin changes (#214)
    * adding activity logger
    
    * update for ativity logger for features
    
    * fix for feature
    
    * add comment about global scope
    
    * update test
    
    * add config file for visiblity

--- app/Http/Controllers/Api/V1/FeatureController.php
+++ app/Http/Controllers/Api/V1/FeatureController.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\Controller;
 use Laravel\Pennant\Feature;
 use App\Traits\Responses;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
 
 class FeatureController extends Controller
 {
@@ -40,16 +41,35 @@ class FeatureController extends Controller
         }
 
         try {
-            $input = $request->only(['enabled']);
-            if ($input['enabled']) {
-                Feature::activateForEveryone($name);
-                return $this->OKResponse([]);
-            }
-
-            Feature::deactivateForEveryone($name);
-            return $this->OKResponse([]);
-        } catch (\Throwable $e) {
-            return $this->NotFoundResponse();
+            $validated = $request->validate([
+                'enabled' => ['required', 'boolean'],
+            ]);
+        } catch (ValidationException $e) {
+            return $this->ValidationErrorResponse($e->errors());
         }
+
+        // Global scope for now, may enable user scoping in the future
+        // $scope = Auth::user();
+        $scope = null;
+        $before = (bool) Feature::for($scope)->value($name);
+        $after = (bool) $validated['enabled'];
+
+        if ($after) {
+            Feature::activateForEveryone($name);
+        } else {
+            Feature::deactivateForEveryone($name);
+        }
+
+        activity('feature_flags')
+            ->causedBy(Auth::user())
+            ->withProperties([
+                'feature' => $name,
+                'before' => ['enabled' => $before],
+                'after' => ['enabled' => $after],
+            ])
+            ->log('feature_flag_updated');
+
+        return $this->OKResponse([]);
+
     }
 }
